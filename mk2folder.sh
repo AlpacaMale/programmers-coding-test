d0ee0b0576533d52f2d14fb32f58385fd3e030d7
AlpacaMale <alpaca9100@gmail.com>
2024-10-11 16:36:02 +0900

    스크립트가 공백도 처리하도록 처리

--- mk2folder.sh
+++ mk2folder.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/zsh
 
 if [ $# -eq 1 ]; then
-    mkdir $1
-    touch $1/$1.py
-    echo "# [][1]" > $1/$1.md
-    echo "입니다." >> $1/$1.md
-    echo "" >> $1/$1.md
-    echo "## 풀이" >> $1/$1.md
-    echo "" >> $1/$1.md
-    echo "[1]: " >> $1/$1.md
+    title=$(echo "$1" | sed 's/ /_/g')
+    mkdir ${title}
+    touch ${title}/${title}.py
+    echo "# [][1]" > ${title}/${title}.md
+    echo "입니다." >> ${title}/${title}.md
+    echo "" >> ${title}/${title}.md
+    echo "## 풀이" >> ${title}/${title}.md
+    echo "" >> ${title}/${title}.md
+    echo "[1]: " >> ${title}/${title}.md
 else
     echo "Usage: $0 foldername"
     exit 1
