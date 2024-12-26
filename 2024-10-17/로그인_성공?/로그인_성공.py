3b196012efeaa70915495d8c1bdc245d0163fea3
박병준 <77188449+AlpacaMale@users.noreply.github.com>
2024-12-26 16:07:49 +0900

    Delete 2024-10-17/로그인_성공? directory

--- 2024-10-17/로그인_성공?/로그인_성공.py
+++
@@ -1,6 +0,0 @@
-def solution(id_pw, db):
-    _id, passwd = id_pw
-    for db_id, db_passwd in db:
-        if db_id == _id:
-            return "login" if passwd == db_passwd else "wrong pw"
-    return "fail"
